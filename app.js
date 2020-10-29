45c57f9af3abead0372ad84e0d6e472856440b0b
IdoGo15 <idogolan15@gmail.com>
2020-10-29 14:46:34 +0200

    finished with final bmi result and showing it

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 let sex = document.getElementById('sex');
 let height = document.getElementById('height');
 let weight = document.getElementById('weight');
-let bmi = document.getElementsByClassName('final-result');
+let bmi = document.getElementById('final-result');
 let form = document.getElementById('bmi-form');
 
 // Events
 form.addEventListener('submit', function(e){
-  // Loader
-  // document.getElementById('loading').style.display = 'block';
 
-  setTimeout(calculateBMI, 0);
+//Loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateBMI, 1000);
 
   e.preventDefault();
 });
@@ -19,6 +20,16 @@ function calculateBMI (e) {
   let heightM = height.value / 100;
   let final = weight.value / Math.pow(heightM, 2);
   final = final.toFixed(2);
+  console.log(final);
+
+  //Hide loader
+  document.getElementById('loading').style.display = 'none';
+
+  // Show results
+  document.getElementById('results').style.display = 'block';
+
+  // Initialize results
+  bmi.textContent = final;
 }
 
 
